a3e7a81d97a2509a77fc4a4e2841dfd2b0fa54ec
Ankit <adhyani37@gmail.com>
2020-07-05 22:20:00 +0100

    Basic FPS Movement.
    WASD and Mouse to move around.

---
+++ CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeedX = 15.0f;
+    private float rotationSpeedY = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To rotate the camera first we need the access to both Mouse X and Mouse Y.
+        float inputX = Input.GetAxis("Mouse X");
+        float inputY = Input.GetAxis("Mouse Y");
+
+        float resultX = transform.rotation.eulerAngles.x - (inputY * rotationSpeedY * Time.deltaTime);
+        float resultY = transform.rotation.eulerAngles.y + (inputX * rotationSpeedX * Time.deltaTime);
+
+
+        //Apply angles to the camera
+        transform.rotation = Quaternion.Euler(resultX, resultY, 0f);
+        
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float movementSpeed = 5f;
+    [SerializeField]
+    public Transform cameraPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        Vector3 cameraRotation = cameraPos.transform.rotation.eulerAngles;
+
+        cameraRotation.x = cameraRotation.z = 0; //As we only need the mouse horizontal axis movement for movement.
+
+        Vector3 offset = new Vector3(inputX, 0, inputZ) * Time.deltaTime * movementSpeed;
+
+        offset = Quaternion.Euler(cameraRotation) * offset;
+
+        transform.position += offset; 
+    }
+}
